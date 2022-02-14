b25d8d6049d3dc5fc71cf00f89ec2df861e79900
Patrick Sadil <psadil@gmail.com>
2022-02-14 13:52:10 -0500

    initial commit

---
+++ src/biomarkers/cli/run.py
@@ -0,0 +1,9 @@
+# import argparse
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description='update log at https://confluence.a2cps.org/display/DOC/QC+Log')
+#     parser.add_argument("img",  nargs="+", help="T1w images to process")
+
+#     args = parser.parse_args()
+#     main(args.img)
+
